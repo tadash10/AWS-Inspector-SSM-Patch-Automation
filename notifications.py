b46509aa46474fe54b6b05cfff696d2805a99ad5
T1 <126980610+tadash10@users.noreply.github.com>
2023-09-12 01:04:40 +0000

    Create notifications.py

---
+++ notifications.py
@@ -0,0 +1,10 @@
+import smtplib
+
+def send_email_notification(subject, message):
+    # Function to send email notifications
+
+def send_sms_notification(message):
+    # Function to send SMS notifications
+
+def send_sns_notification(message):
+    # Function to send AWS SNS notifications
